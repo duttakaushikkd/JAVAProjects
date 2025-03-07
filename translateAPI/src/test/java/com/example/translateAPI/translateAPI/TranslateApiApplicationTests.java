1d0651be891dbc8bf2716750563c1dcd0f88e07e
duttakaushikkd <duttakaushikkd@gmail.com>
2025-03-08 04:10:33 +0530

    Translate API - Initial commit

---
+++ translateAPI/src/test/java/com/example/translateAPI/translateAPI/TranslateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.translateAPI.translateAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
